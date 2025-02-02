251cae7e5e2d2b7bdfe8f6448d8d308afb374c0e
coding-z <112389645+coding-z@users.noreply.github.com>
2025-02-02 16:22:19 +1100

    Add bubble sort with marker for sorted sublist

--- bubble_sort.py
+++ bubble_sort.py
@@ -7,3 +7,14 @@ def naive_bubble_sort(vals: list[int]) -> list[int]:
                 vals[i], vals[i + 1] = vals[i + 1], vals[i]
     
     return vals
+
+# Bubble sort with marker for sorted sublist
+
+def marked_bubble_sort(vals: list[int]) -> list[int]:
+    for mark in range(len(vals) - 1, 0, -1):
+        for i in range(mark):
+            if vals[i] > vals[i + 1]:
+                vals[i], vals[i + 1] = vals[i + 1], vals[i]
+        mark -= 1
+    
+    return vals
