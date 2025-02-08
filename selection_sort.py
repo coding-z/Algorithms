6cdb143367ec455d85735fadf0040ff7b6273956
coding-z <112389645+coding-z@users.noreply.github.com>
2025-02-08 11:40:10 +1100

    Format selection sort docstring

--- selection_sort.py
+++ selection_sort.py
@@ -1,16 +1,20 @@
-# Selection sort
-#           Worst   Best
-# Time      O(n^2)  O(n^2)
-# Aux Space O(1)    O(1)
-# Not incremental as appending or prepending breaks invariant
-# Invariant: sorted sublist at front which is all smaller than unsorted sublist
-# Unstable as swapping non-adjacent elements
+# Selection Sort Algorithm
 
-def selection_sort(vals: list[int]) -> list[int]:
-    for i in range(len(vals) - 1):
+def selection_sort(list):
+    """Sorts list in ascending order with selection sort.
+    
+    Complexity  Best    Worst
+    Time        O(n^2)  O(n^2)
+    Aux Space   O(1)    O(1)
+
+    Invariant: Sorted sublist at front which is smaller than remaining list.
+
+    Not incremental as appending or prepending breaks invariant.
+    Unstable as swapping of non-adjacent items occurs.
+    """
+    for i in range(len(list) - 1):
         min_index = i
-        for j in range(i + 1, len(vals)):
-            if vals[j] < vals[min_index]:
+        for j in range(i + 1, len(list)):
+            if list[j] < list[min_index]:
                 min_index = j
-        vals[i], vals[min_index] = vals[min_index], vals[i]
-    return vals
+        list[i], list[min_index] = list[min_index], list[i]
