fea945c7d9cc7371cecee879009f8940703f6b19
coding-z <112389645+coding-z@users.noreply.github.com>
2025-02-08 11:33:20 +1100

    Format and comment bubble sort algorithms

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,50 +1,57 @@
-# Naive bubble sort
-#           Worst   Best
-# Time      O(n^2)  O(n^2)
-# Aux Space O(1)    O(1)
-# Incremental if prepend
-# Stable
+# Bubble Sort Algorithms
 
-def naive_bubble_sort(vals: list[int]) -> list[int]:
-    for _ in range(len(vals) - 1):
-        for i in range(len(vals) - 1):
-            if vals[i] > vals[i + 1]:
-                vals[i], vals[i + 1] = vals[i + 1], vals[i]
+def bubble_sort_naive(list):
+    """Naive unoptimised bubble sort.
     
-    return vals
+    Complexity  Best    Worst
+    Time        O(n^2)  O(n^2)
+    Aux Space   O(1)    O(1)
 
-# Bubble sort with marker for sorted sublist
-#           Worst   Best
-# Time      O(n^2)  O(n^2)
-# Aux Space O(1)    O(1)
-# Incremental if prepend
-# Stable
+    Incremental if prepending.
+    Stable with strict comparison.
+    """
+    for _ in range(len(list) - 1):
+        for i in range(len(list) - 1):
+            if list[i] > list[i + 1]:
+                list[i], list[i + 1] = list[i + 1], list[i]
 
+def bubble_sort_marked(list):
+    """Bubble sort without searching sorted items.
+    
+    Complexity  Best    Worst
+    Time        O(n^2)  O(n^2)
+    Aux Space   O(1)    O(1)
+    
+    Incremental if prepending.
+    Stable with strict comparison.
 
-def marked_bubble_sort(vals: list[int]) -> list[int]:
-    for mark in range(len(vals) - 1, 0, -1):
+    In each iteration the largest unsorted item is moved to it's correct
+    place. This item can be avoided in the next iteration as it is
+    already correctly in place.
+    """
+    for mark in range(len(list) - 1, 0, -1):
         for i in range(mark):
-            if vals[i] > vals[i + 1]:
-                vals[i], vals[i + 1] = vals[i + 1], vals[i]
-        mark -= 1
+            if list[i] > list[i + 1]:
+                list[i], list[i + 1] = list[i + 1], list[i]
+
+def bubble_sort_optimised(list):
+    """Bubble sort optimised to linear best-case time.
     
-    return vals
+    Complexity  Best    Worst
+    Time        O(n)    O(n^2)
+    Aux Space   O(1)    O(1)
+    
+    Incremental if prepending.
+    Stable with strict comparison.
 
-# Bubble sort with detection for when there are no more swaps
-#           Worst   Best
-# Time      O(n^2)  O(n)
-# Aux Space O(1)    O(1)
-# Incremental if prepend
-# Stable
-
-
-def bubble_sort_2(vals: list[int]) -> list[int]:
-    for mark in range(len(vals) - 1, 0, -1):
+    If no swaps occur in the unsorted sublist, then they are already in sorted
+    order. No more iterations are needed afterwards.
+    """
+    for mark in range(len(list) - 1, 0, -1):
         swapped = False
         for i in range(mark):
-            if vals[i] > vals[i + 1]:
-                vals[i], vals[i + 1] = vals[i + 1], vals[i]
+            if list[i] > list[i + 1]:
+                list[i], list[i + 1] = list[i + 1], list[i]
                 swapped = True
         if not swapped:
             break
-    return vals
