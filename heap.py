0d390cd42b95fb5880dd5ff6184485c0f3111dc5
coding-z <112389645+coding-z@users.noreply.github.com>
2025-02-14 16:29:52 +1100

    Update linked heap to array heap

--- heap.py
+++ heap.py
@@ -1,11 +1,11 @@
-"""Heap data structure implemented using linked nodes."""
+"""Heap data structure implemented using an array."""
 
 class MaxHeap:
     """A max heap which stores the largest value at the root."""
 
     def __init__(self):
-        self.root = None
+        self.array = []
     
     def add(self, value):
         """Adds an item to the heap."""
-        
+        pass
