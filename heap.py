63dce94fd61871b9594a04048200fc163fe8b127
coding-z <112389645+coding-z@users.noreply.github.com>
2025-02-15 13:09:30 +1100

    Work on heap

--- heap.py
+++ heap.py
@@ -18,3 +18,22 @@ class MaxHeap:
             self.array[index] = buffer
             index = parent
             parent = (index - 1) // 2
+    
+    def pop_max(self):
+        """Removes and returns the largest heap item."""
+        # Swap the max and last items
+        buffer = self.array[0]
+        self.array[0] = self.array[-1]
+        self.array[-1] = buffer
+
+        value = self.array.pop()
+
+        index = 0
+        child1 = 2 * index + 1
+        child2 = child1 + 1
+
+        if child2 < len(self.array) and self.array[child1] > self.array[child2]:
+
+        while self.array[index] < max(self.array[child1], self.array[child2]):
+            if self.array[child1] > self.array[child2]:
+
